26de66babafe57b9a5f6885de240ab5fc1dbcbee
Guillermo Sanchez <gsdionis@gmail.com>
2025-07-24 14:51:05 +0200

    add ruff

--- python/app.py
+++ python/app.py
@@ -1,4 +1,11 @@
-from backend import list_table_json_ser, get_table_stats_column_json_ser, get_table_stats_row_json_ser, get_table_stats_arrow_ser, get_nrows_json_ser, get_nrows_arrow_ser
+from backend import (
+    list_table_json_ser,
+    get_table_stats_column_json_ser,
+    get_table_stats_row_json_ser,
+    get_table_stats_arrow_ser,
+    get_nrows_json_ser,
+    get_nrows_arrow_ser,
+)
 from fastapi import FastAPI, Response
 from fastapi.responses import StreamingResponse
 
@@ -13,27 +20,42 @@ def list_tables():
 @app.get("/stats/json/{table_name}")
 def get_table_stats_json(table_name: str, format: str = "row"):
     if format == "row":
-        return Response(content=get_table_stats_row_json_ser(table_name), media_type="application/json")
+        return Response(
+            content=get_table_stats_row_json_ser(table_name),
+            media_type="application/json",
+        )
     elif format == "column":
-        return Response(content=get_table_stats_column_json_ser(table_name), media_type="application/json")
+        return Response(
+            content=get_table_stats_column_json_ser(table_name),
+            media_type="application/json",
+        )
     else:
-        return Response(content="Invalid format specified. Use 'row' or 'column'.", media_type="text/plain", status_code=400)
-    
+        return Response(
+            content="Invalid format specified. Use 'row' or 'column'.",
+            media_type="text/plain",
+            status_code=400,
+        )
+
+
 @app.get("/stats/arrow/{table_name}")
 def get_table_stats_arrow(table_name: str):
     # Placeholder for Arrow format implementation
     return StreamingResponse(
         get_table_stats_arrow_ser(table_name),
-        media_type="application/vnd.apache.arrow.stream"
+        media_type="application/vnd.apache.arrow.stream",
     )
+
 
 @app.get("/rows/json/{table_name}")
 def get_nrows_json(table_name: str, nrows: int = 10000):
-    return Response(content=get_nrows_json_ser(table_name, nrows), media_type="application/json")
+    return Response(
+        content=get_nrows_json_ser(table_name, nrows), media_type="application/json"
+    )
+
 
 @app.get("/rows/arrow/{table_name}")
 def get_nrows_arrow(table_name: str, nrows: int = 10000):
     return StreamingResponse(
         get_nrows_arrow_ser(table_name, nrows),
-        media_type="application/vnd.apache.arrow.stream"
+        media_type="application/vnd.apache.arrow.stream",
     )
--- python/backend.py
+++ python/backend.py
@@ -11,6 +11,7 @@ from typing import Generator
 ### JSON Serialization ###
 ##########################
 
+
 class NumpyArrayEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, numpy.ndarray):
@@ -19,70 +20,91 @@ class NumpyArrayEncoder(json.JSONEncoder):
             return obj.isoformat()
         return json.JSONEncoder.default(self, obj)
 
-def list_table_json_ser()-> str:
-    with duckdb.connect('../db/database.duckdb') as conn:
+
+def list_table_json_ser() -> str:
+    with duckdb.connect("../db/database.duckdb") as conn:
         result = conn.execute("SHOW TABLES").fetchall()
-    
+
     result_list = [row[0] for row in result]
     return json.dumps(result_list)
 
+
 def get_table_stats_row_json_ser(table_name: str) -> str:
-    with duckdb.connect('../db/database.duckdb') as conn:
+    with duckdb.connect("../db/database.duckdb") as conn:
         cursor = conn.execute(f"SUMMARIZE {table_name}")
         data = cursor.fetchall()
         column_names = [desc[0] for desc in cursor.description]
 
     # Convert the data to a list of dictionaries to make it row oriented, then serialize to JSON
     result_dict = []
-    for row in data:    
-        row_dict = {column_names[i]: float(row[i]) if isinstance(row[i], Decimal) else row[i] for i in range(len(column_names))}
+    for row in data:
+        row_dict = {
+            column_names[i]: float(row[i]) if isinstance(row[i], Decimal) else row[i]
+            for i in range(len(column_names))
+        }
         result_dict.append(row_dict)
     return json.dumps(result_dict)
 
+
 def get_table_stats_column_json_ser(table_name: str) -> str:
-    with duckdb.connect('../db/database.duckdb') as conn:
+    with duckdb.connect("../db/database.duckdb") as conn:
         result = conn.execute(f"SUMMARIZE {table_name}").fetchnumpy()
-    
+
     # Use custom numpy serializer
     return json.dumps(result, cls=NumpyArrayEncoder)
 
+
 def get_nrows_json_ser(table_name: str, nrows: int = 10000) -> str:
-    with duckdb.connect('../db/database.duckdb') as conn:
+    with duckdb.connect("../db/database.duckdb") as conn:
         result = conn.execute(f"SELECT * FROM {table_name} LIMIT {nrows}").fetchnumpy()
-    
+
     # Use custom numpy serializer
     return json.dumps(result, cls=NumpyArrayEncoder)
+
 
 ###########################
 ### Arrow Serialization ###
 ###########################
 
-def get_table_stats_arrow_ser(table_name: str):
+
+def get_table_stats_arrow_ser(table_name: str) -> Generator[io.BytesIO, None, None]:
     """
     Returns a generator that yields Arrow serialized statistics for the specified table.
     The generator yields memoryview objects from buffers
     """
-    with duckdb.connect('../db/database.duckdb') as conn:
-        batch_reader = conn.execute(f"SUMMARIZE {table_name}").fetch_record_batch() # This is throwing a weird error
+    with duckdb.connect("../db/database.duckdb") as conn:
+        batch_reader = conn.execute(
+            f"SUMMARIZE {table_name}"
+        ).fetch_record_batch()  # This is throwing a weird error
         return _arrow_reader_to_ipc_stream(batch_reader)
 
-def get_nrows_arrow_ser(table_name: str, nrows: int = 10000) -> Generator[io.BytesIO, None, None]:
+
+def get_nrows_arrow_ser(
+    table_name: str, nrows: int = 10000
+) -> Generator[io.BytesIO, None, None]:
     """
     Returns a generator that yields Arrow serialized rows for the specified table.
     The generator yields memoryview objects from buffers
     """
-    with duckdb.connect('../db/database.duckdb') as conn:
-        batch_reader = conn.execute(f"SELECT * FROM {table_name} LIMIT {nrows}").fetch_record_batch()
+    with duckdb.connect("../db/database.duckdb") as conn:
+        batch_reader = conn.execute(
+            f"SELECT * FROM {table_name} LIMIT {nrows}"
+        ).fetch_record_batch()
         return _arrow_reader_to_ipc_stream(batch_reader)
 
-def _arrow_reader_to_ipc_stream(batch_reader: pa.RecordBatchReader) -> Generator[io.BytesIO, None, None]:
+
+def _arrow_reader_to_ipc_stream(
+    batch_reader: pa.RecordBatchReader,
+) -> Generator[io.BytesIO, None, None]:
     """
     Converts a RecordBatchReader to an IPC stream in a BytesIO buffer.
     """
     arrow_schema = batch_reader.schema
-    with batch_reader as source, \
-        io.BytesIO() as sink, \
-        pa.ipc.new_stream(sink, arrow_schema) as writer:
+    with (
+        batch_reader as source,
+        io.BytesIO() as sink,
+        pa.ipc.new_stream(sink, arrow_schema) as writer,
+    ):
         for batch in source:
             sink.seek(0)
             writer.write_batch(batch)
